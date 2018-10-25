77a36e7da963e380c3f025504a598afa2a546552
Victor Juan Bernardo Quintanilha <victorjuan_7@hotmail.com>
2018-10-25 19:17:32 -0300

    Aula 18/10 - Segunda parte (Cópia)

--- Aulas/Aula1810_Condicionais/Aula1810_Condicionais.cpp
+++ Aulas/Aula1810_Condicionais/Aula1810_Condicionais.cpp
@@ -1,44 +1,67 @@
 #include "stdafx.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
-/*
-"e" - &&		verdadeiro somente quando todas condicoes forem verdadeiras
-"ou" - ||		basta que uma condicao seja verdadeira para ser verdadeiro
-*/
-int lernumerointeiro()
+#include <stdio.h>
+int LerNumeroInteiro()
 {
-	int numero;
-	printf("Favor digitar um número inteiro: ");
-	scanf_s("%i", &numero);
-	return numero;
+	int a;
+	printf("Favor digitar um numero inteiro: ");
+	scanf_s("%i", &a);
+	return a;
 }
-int maiorvalor(int x, int y, int z, int w)
+int MaiorValorInteiro(int a, int b, int c, int d)
 {
-	int verificador = x;
-	if (y > verificador)
+	int maior = a;
+	if (maior < b)
 	{
-		verificador = y;
+		maior = b;
 	}
-	if (z > verificador)
+	if (maior < c)
 	{
-		verificador = z;
+		maior = c;
 	}
-	if (w > verificador)
+	if (maior < d)
 	{
-		verificador = w;
+		maior = d;
 	}
-	return verificador;
+	return maior;
+}
+float CalculaMedia(float a, float b, float c, float d)
+{
+	float soma = a + b + c + d;
+	float media = soma / 4;
+	return media;
+}
+void ImprimeValoresMaioresQueMedia(int a, int b, int c, int d, float media)
+{
+	printf("Os valores maiores que a media sao: ");
+	if (a > media)
+	{
+		printf("%i ", a);
+	}
+	if (b > media)
+	{
+		printf("%i ", b);
+	}
+	if (c > media)
+	{
+		printf("%i ", c);
+	}
+	if (d > media)
+	{
+		printf("%i ", d);
+	}
 }
 int main()
 {
-	setlocale(LC_ALL, "Portuguese");
-	int primeirovalor = lernumerointeiro();
-	int segundovalor = lernumerointeiro();
-	int terceirovalor = lernumerointeiro();
-	int quartovalor = lernumerointeiro();
-	int a = maiorvalor(primeirovalor, segundovalor, terceirovalor, quartovalor);
-	printf("\nO maior número é: %i\n\n", a);
+	int primeiro = LerNumeroInteiro();
+	int segundo = LerNumeroInteiro();
+	int terceiro = LerNumeroInteiro();
+	int quarto = LerNumeroInteiro();
+	int m = MaiorValorInteiro(primeiro, segundo, terceiro, quarto);
+	printf("Maior valor encontrado: %i\n\n", m);
+	float mediaValores = CalculaMedia((float)primeiro, (float)segundo, (float)terceiro, (float)quarto);
+	printf("A media dos valores digitados eh: %f\n", (float)mediaValores);
+	ImprimeValoresMaioresQueMedia(primeiro, segundo, terceiro, quarto, mediaValores);
 	system("pause");
 	return 0;
 }
